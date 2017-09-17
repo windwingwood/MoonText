9483f30a593a80e27f4153641e0e296bc762ccfe
陈彦朴 <windwingwood@qq.com>
2017-09-17 21:17:46 +0800

    3.0 修正了一些错误，新增多密钥管理，支持切换

--- output.js
+++ output.js
@@ -13,6 +13,20 @@ function error(String){
 function clean(){
 		document.getElementById("output").innerHTML="";
 }
+function refreshSelect(){
+		document.getElementById("keySelect").innerHTML="";
+		var keyCount =  localGet("keyCount");
+		var innerText,keyText,temp;
+		for(var i=0;i<keyCount;i++){
+				keyText = localGet("key"+i);
+				temp = keyText.indexOf("/");
+				keyText = keyText.substring(0,temp);
+				temp = document.createElement("option");
+				temp.innerHTML = keyText;
+				temp.value = i;
+				document.getElementById("keySelect").appendChild(temp);
+		}
+}
 function clipSet(t){
 		if(navigator.userAgent.indexOf("MSIE") == -1){
 				alert("该浏览器不支持！");
@@ -33,9 +47,22 @@ function clipGet(){
 		return 0;
 }
 function localSet(name, value){
-		localStorage.setItem(name,value);
+		if(name=="key"){
+				var keyCount = localGet("keyCount");
+				localStorage.setItem(name,value);
+				localStorage.setItem("key"+keyCount,value);
+				localStorage.setItem("keyCount",keyCount+1);
+		}else localStorage.setItem(name,value);
+}
+function localKey(value){
+		localStorage.setItem("key",value);
 }
 function localGet(name){
+		if(name=="keyCount"){
+				var temp = parseInt(localStorage.getItem("keyCount"));
+				if(isNaN(temp))return 0;
+				else return temp;
+		}
 		return localStorage.getItem(name);
 }
 function cookieSet(name, value){
